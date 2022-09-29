a3c23eff3c2a4cedf5ec48a21d1654bff7e54b04
MdNaimRipto <naimurtsc567@gmail.com>
2022-09-29 20:36:59 +0600

    footer completed

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>All Right Reserved 2022-2025 by Fitness-Club.</p>
+        </div>
+    );
+};
+
+export default Footer;
