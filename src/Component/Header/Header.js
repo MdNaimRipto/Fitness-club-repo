e35723323ce87cc7e6b720dd9ed29046f0f44ee2
MdNaimRipto <naimurtsc567@gmail.com>
2022-09-29 09:19:59 +0600

    Header section completed

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,11 +1,15 @@
 import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import "./Header.css"
 
 const Header = () => {
     return (
         <div>
-            <h1><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon> Fitness Club</h1>
+            <h1 className='header'>
+                <FontAwesomeIcon className='header-icon' icon={faDumbbell}></FontAwesomeIcon>
+                Fitness <span>Club</span>
+            </h1>
         </div>
     );
 };
